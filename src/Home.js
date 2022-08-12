520b2a211628795175f8032c0d5f84216667e217
mrzh-ir <m.iranshahi67@gmail.com>
2022-08-12 14:53:29 +0200

    render blog posts

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,19 @@ import { useState } from 'react';
 
 const Home = () => {
 
-    const [currentnumber, setNumber] = useState(0);
-    const handleClick = () => {
-        setNumber(currentnumber + 10);
-    }
-
+    const [blogs, setBlogs] = useState([
+        { title : 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
+        { title : 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+        { title : 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
+    ]);
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p> {currentnumber} </p>
-            <button onClick = {handleClick}>Click me!</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2> {blog.title} </h2>
+                    <p> Written by {blog.author} </p>
+                </div>
+            ))}
         </div>
     );
 }
