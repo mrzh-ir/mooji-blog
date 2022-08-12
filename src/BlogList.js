01cb276f60a4ed0e3c261c7dad623ea84b9f24ce
mrzh-ir <m.iranshahi67@gmail.com>
2022-08-12 15:12:56 +0200

    props

---
+++ src/BlogList.js
@@ -0,0 +1,18 @@
+// props is a way to pass the data from parent component to child component
+const BlogList = ( {blogs, title}) => {
+    // const blogs = props.blogs;
+    // const title = props.title;
+    return ( 
+        <div className="blog-list">
+        <h2> { title } </h2>
+        {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <h2> {blog.title} </h2>
+                <p> Written by {blog.author} </p>
+            </div>
+        ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
