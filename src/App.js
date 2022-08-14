2c7395ca4d6529bd7d4697e7ee25875d3bf5a2ad
mrzh-ir <m.iranshahi67@gmail.com>
2022-08-14 16:39:31 +0200

    install react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // App is our t-root component
 function App() {
 
   // jsx template
   return (
+    <Router>
     <div className="App">
       <Navbar/>
       <div className="content">
-        <h1><Home/></h1>
+        <Switch>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
