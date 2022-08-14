813fe8189e30243319c5b1954e5b91648fa89749
mrzh-ir <m.iranshahi67@gmail.com>
2022-08-14 13:18:30 +0200

    useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,32 @@
-import { useState } from 'react';
-import BlogList from './BlogList';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
-    const [blogs, setBlogs] = useState([
-        { title : 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-        { title : 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        { title : 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
-    ]);
+  const [name, setName] = useState('mario');
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-    return (
-        <div className="home">
-            <BlogList blogs = {blogs} title="All Blogs" handleDelete={handleDelete}/>
-        </div>
-    );
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  useEffect(() => {
+    console.log('use effect ran');
+  }, [name]);
+  //[] is dependency, it fires when name is changed
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={(() => setName('loigi'))}>change name</button>
+      <p> {name} </p>
+    </div>
+  );
 }
  
 export default Home;
