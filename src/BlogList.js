905097f4b73c129b91b616d496ca2bd1f91d4ee0
mrzh-ir <m.iranshahi67@gmail.com>
2022-08-14 15:38:01 +0200

    fetching data with useEffect

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,5 +1,5 @@
 // props is a way to pass the data from parent component to child component
-const BlogList = ( {blogs, title, handleDelete}) => {
+const BlogList = ( {blogs, title}) => {
     return ( 
         <div className="blog-list">
         <h2> { title } </h2>
@@ -7,7 +7,6 @@ const BlogList = ( {blogs, title, handleDelete}) => {
             <div className="blog-preview" key={blog.id}>
                 <h2> {blog.title} </h2>
                 <p> Written by {blog.author} </p>
-                <button onClick={() => handleDelete(blog.id)}> delete blogs</button>
             </div>
         ))}
         </div>
