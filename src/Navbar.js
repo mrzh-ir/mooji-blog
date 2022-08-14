f8004fbe52ce3052158b36119ae922a61eec9d55
mrzh-ir <m.iranshahi67@gmail.com>
2022-08-14 17:00:47 +0200

    Link in router

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,12 @@
-// a component is just a function that returns a jsx template and that function is exported at the bottom of the file
-// sfc+tab : stateless functional component
-// { } means dynamic value
-// {{ }} means object inside dynamic value
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Mooji Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create"> New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create"> New Blog</Link>
             </div>
         </nav>
     );
